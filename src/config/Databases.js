9920bdb4c509da1c2c5a6f06b84533473a04fb93
jhaturaphat <umt.eng52@gmail.com>
2022-03-06 22:29:43 +0700

    databases

--- src/config/Databases.js
+++ src/config/Databases.js
@@ -12,6 +12,16 @@ class Databases {
             port:process.env.DB_PORT
         })
     }
+
+    // Custom ฟังชั่นก์ Query ข้อมูลใหม่
+    query(sql, params = null) {
+        return new Promise((resolve, reject) => {           
+            this.connection.query(sql, params, (errors, result) => {
+                if (errors) return reject({ errors });                
+                resolve(result);
+            });
+        });
+    }
 }
 
 module.exports = {Databases}
--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const WebSocket = require('ws')
 const aruba_tmp_proto = require("./aruba_iot_proto").aruba_telemetry;
+const databases = require("./config/Databases")
 
+const db = databases.Databases
+
+const _db = new db();
 
 const wss = new WebSocket.Server({
     port: process.env.PORT,
@@ -10,25 +14,37 @@ const wss = new WebSocket.Server({
 // สร้าง websocket server 
 wss.on("connection", (ws)=>{
     ws.on('message', (message)=>{
-        // รอรับข้อมูลจาก Aruba ที่ส่งมาแบบตลอดเวลา
-        let telemetryReport = aruba_tmp_proto.Telemetry.decode(message)
-        
-        let obj = JSON.parse(JSON.stringify(telemetryReport))
-        console.log("Befor+++++++++++");
-        console.log(obj);
-        console.log("EndBefor+++++++++++");
+        try {
+            // รอรับข้อมูลจาก Aruba ที่ส่งมาแบบตลอดเวลา
+            let telemetryReport = aruba_tmp_proto.Telemetry.decode(message)
+            let obj = JSON.parse(JSON.stringify(telemetryReport))
+            console.log("###Befor###");
+            console.log(obj);
+            console.log("###EndBefor###");           
+            
 
         if(!obj.hasOwnProperty("reported")){
+            
              console.log("Aruba Websocket Established");
         }else{
-            console.log("++++++TO++++++++++");
+
+            this.db.query("INSERT INTO ",[dbj])
+            console.log("++++++TO+++++");
             console.log(obj["reported"]); //
             console.log(obj["reporter"]["name"]); //ชื่อ Access point
-            console.log("+++++++END++++++++++")
+            console.log("+++++++END++++++")
             console.log("If=================");
             console.log(obj);
             console.log("EnIf=================");
         }
+
+    } catch (error) {
+        // console.log(error);   
+        
+        _db.query("INSERT INTO device_inventory (d_name, d_macaddress) VALUES (?,?)", ["my name","macaddress"]).then(()=>{
+
+       });     
+    }    
 
     })
     ws.on('close', function close() {
