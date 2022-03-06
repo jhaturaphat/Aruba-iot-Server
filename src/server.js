286f9734741ed8ab70a7b662a0597305ff3ad83f
jhaturaphat <umt.eng52@gmail.com>
2022-03-06 09:05:28 +0700

    xxxx

--- src/server.js
+++ src/server.js
@@ -1,16 +1,33 @@
 require('dotenv').config();
-const websocket = require('ws')
+const WebSocket = require('ws')
 const aruba_tmp_proto = require("./aruba_iot_proto").aruba_telemetry;
 
 
 const wss = new WebSocket.Server({
     port: process.env.PORT,
     path:"/aruba"
-})
+});
 // สร้าง websocket server 
 wss.on("connection", (ws)=>{
     ws.on('message', (message)=>{
+        // รอรับข้อมูลจาก Aruba ที่ส่งมาแบบตลอดเวลา
+        let telemetryReport = aruba_tmp_proto.Telemetry.decode(message)
         
+        let obj = JSON.parse(JSON.stringify(telemetryReport))
+
+        console.log(obj);
+
     })
+    ws.on('close', function close() {
+        // จะทำงานเมื่อปิด Connection ในตัวอย่างคือ ปิด Browser
+          console.log('disconnected');
+        });
+    ws.send('init message to client');
+        // ส่ง data ไปที่ client เชื่อมกับ websocket server นี้
+    
 })
+wss.on('listening', ()=>{
+    console.log(`server is listening on port ${process.env.PORT}`);
+  })
+  
 
